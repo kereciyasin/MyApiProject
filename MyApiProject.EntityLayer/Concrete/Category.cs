45d8e16fd1fe0b06c66ee17811d1ab92a1f4d88e
Yasin Kereci <yke144907@stud.gibb.ch>
2025-06-22 18:19:37 +0200

    ...

---
+++ MyApiProject.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApiProject.EntityLayer.Concrete
+{
+    public class Category
+    {
+        public int CagoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
---
+++ MyApiProject.EntityLayer/Concrete/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApiProject.EntityLayer.Concrete
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Price { get; set; }
+        public decimal Stock { get; set; }
+        public int CategoryId { get; set; } // Foreign key to Category
+        public Category Category { get; set; } // Navigation property
+    }
+}
