84710a6b9600ea1d63b9b7241ed8cb53aed419a7
Yasin Kereci <yke144907@stud.gibb.ch>
2025-06-22 18:42:42 +0200

    refactor(data-access): Refactor GenericRepository to use ApiContext with dependency injection
    - Updated GenericRepository<T> to accept ApiContext via constructor injection
    - Implemented CRUD methods using EF Core's DbSet<T>

--- MyApiProject.DataAccessLayer/Repositories/GenericRepository.cs
+++ MyApiProject.DataAccessLayer/Repositories/GenericRepository.cs
@@ -1,4 +1,5 @@
 ﻿using MyApiProject.DataAccessLayer.Abstract;
+using MyApiProject.DataAccessLayer.Context;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,29 +10,40 @@ namespace MyApiProject.DataAccessLayer.Repositories
 {
     public class GenericRepository<T> : IGenericDal<T> where T : class
     {
+        private readonly ApiContext _context;
+
+        public GenericRepository(ApiContext context)
+        {
+            _context = context;
+        }
+
         public void Delete(int id)
         {
-            throw new NotImplementedException();
+            var value = _context.Set<T>().Find(id);
+            _context.Set<T>().Remove(value);
+            _context.SaveChanges();
         }
 
         public List<T> GetAll()
         {
-            throw new NotImplementedException();
+            return _context.Set<T>().ToList();
         }
 
         public T GetById(int id)
         {
-            throw new NotImplementedException();
+            return _context.Set<T>().Find(id);
         }
 
         public void Insert(T entity)
         {
-            throw new NotImplementedException();
+            _context.Set<T>().Add(entity);
+            _context.SaveChanges();
         }
 
         public void Update(T entity)
         {
-            throw new NotImplementedException();
+            _context.Set<T>().Update(entity);
+            _context.SaveChanges();
         }
     }
 
