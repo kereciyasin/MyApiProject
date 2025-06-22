75eabe6be253c49529c7c85c09ba3a8fa5e7d7d4
Yasin Kereci <yke144907@stud.gibb.ch>
2025-06-22 18:37:52 +0200

    feat(data-access): Add generic repository implementation in Repositories folder
    - Created GenericRepository<T> class implementing IGenericDal<T>

---
+++ MyApiProject.DataAccessLayer/Context/ApiContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApiProject.DataAccessLayer.Context
+{
+    public class ApiContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=MyApiProjectDb;User Id=myuser;Password=mypassword;");
+        }
+    }
+}
