09eed0b2f076a903a685561d165d6dd17fe6ded2
Yasin Kereci <yke144907@stud.gibb.ch>
2025-06-23 16:48:52 +0200

    feat(dto): Add CreateProductDto and UpdateProductDto for product operations
    - Added DTOs for creating and updating products in ProductDtos folder
    - Prepares API for clean and secure data transfer in product endpoints

---
+++ MyApiProject.DtoLayer/ProductDtos/CreateProductDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApiProject.DtoLayer.ProductDtos
+{
+    public class CreateProductDto
+    {
+        public string ProductName { get; set; }
+        public int Price { get; set; }
+        public decimal Stock { get; set; }
+        public int CategoryId { get; set; } // Foreign key to Category
+    }
+}
---
+++ MyApiProject.DtoLayer/ProductDtos/UpdateProductDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApiProject.DtoLayer.ProductDtos
+{
+    public class UpdateProductDto
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Price { get; set; }
+        public decimal Stock { get; set; }
+        public int CategoryId { get; set; } // Foreign key to Category
+    }
+}
