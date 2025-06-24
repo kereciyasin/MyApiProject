5d414def42e7c0103da0415d8fe97042453cb632
Yasin Kereci <yke144907@stud.gibb.ch>
2025-06-24 17:16:14 +0200

    chore(webui): Create WEBUI project for front-end integration
    - Added ASP.NET Core MVC project for user interface layer
    - Prepares for API integration and front-end development

---
+++ MyApiProject.WebUI/Controllers/CategoriesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyApiProject.WebUI.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult CategoryList()
+        {
+            return View();
+        }
+    }
+}
