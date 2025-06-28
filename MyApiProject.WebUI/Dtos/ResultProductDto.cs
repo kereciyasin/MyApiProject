1cdaf9599bb39be7b11dd287e2afb48707a904b8
Yasin Kereci <yke144907@stud.gibb.ch>
2025-06-28 09:38:36 +0200

    product dto add

---
+++ MyApiProject.WebUI/Dtos/ResultProductDto.cs
@@ -0,0 +1,14 @@
+﻿using MyApiProject.EntityLayer.Concrete;
+
+namespace MyApiProject.WebUI.Dtos
+{
+    public class ResultProductDto
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Price { get; set; }
+        public decimal Stock { get; set; }
+        public int CategoryId { get; set; } // Foreign key to Category
+        public Category Category { get; set; } // Navigation property
+    }
+}
